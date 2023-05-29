508ab9da1d838399c71eb93d628897e07f4734e8
mwchoen <cheonmyeongwon@gmail.com>
2023-05-29 09:27:58 +0900

    first commit

---
+++ src/main/java/com/example/respringcorebasic/ReSpringCoreBasicApplication.java
@@ -0,0 +1,13 @@
+package com.example.respringcorebasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReSpringCoreBasicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReSpringCoreBasicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/respringcorebasic/ReSpringCoreBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.respringcorebasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReSpringCoreBasicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
